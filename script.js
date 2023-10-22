5213dcd99c07d1f7f418c98bed8189db8e339304
Jonathan Huber <41285866+ValidThought@users.noreply.github.com>
2023-10-22 14:50:28 +0200

    changes html js css

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ new fullpage('#fullpage', {
 function backToTop() {
     fullpage_api.moveTo(1);  // Scrollt zur ersten Section
 }
+
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Funktion zum Hinzufügen der Klasse 'in-view' zu Sektionen im Viewport
+function handleScroll() {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        if (isElementInViewport(section)) {
+            section.classList.add('in-view');
+        } else {
+            section.classList.remove('in-view');
+        }
+    });
+}
+
+// Event-Listener für Scroll-Ereignisse
+window.addEventListener('scroll', handleScroll);
+
+// Initialer Aufruf der handleScroll-Funktion
+handleScroll();
